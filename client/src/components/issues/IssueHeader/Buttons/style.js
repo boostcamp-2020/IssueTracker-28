808b98cde2b8219e8182257247a3ec4799ee2f23
이상경 <sknglee22@naver.com>
2020-10-28 00:08:08 +0900

    [Style] IssueHeader 버튼 공통부분 CSS
    IssueHeader에 있는 버튼들 중 공통되는 디자인을 CommonHeaderButton로 명시한뒤 CSS-in-JS 형태로 코드를 작성

---
+++ client/src/components/issues/IssueHeader/Buttons/style.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+
+const CommonHeaderButton = styled.button`
+  display: flex;
+  box-sizing: border-box;
+  min-width: ${px(200)};
+  height: ${px(50)};
+  border: ${px(1)} solid #f2f2f2;
+  border-radius: ${px(2)};
+  align-items : center;
+  justify-content : center;
+  font-size: ${px(16)};
+  font-weight : 700;
+  cursor: pointer;
+`;
+
+export default {
+};
