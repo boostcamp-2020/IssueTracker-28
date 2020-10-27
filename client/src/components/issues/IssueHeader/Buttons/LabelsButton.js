423f12f1ea5b06a09bc98fcb6c1cedeb88079d10
이상경 <sknglee22@naver.com>
2020-10-28 01:21:18 +0900

    [Feat] IssueHeader 버튼 아이콘 추가
    octicons-react 라이브러리를 사용하여 아이콘 불러오기 & 추가

--- client/src/components/issues/IssueHeader/Buttons/LabelsButton.js
+++ client/src/components/issues/IssueHeader/Buttons/LabelsButton.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import S from './style';
+import {TagIcon} from '@primer/octicons-react'
+
 
 function LabelsButton (){
     return (
         <S.LabelsButton>
+          <TagIcon size={14}/>
             Labels
         </S.LabelsButton>
     );
--- client/src/components/issues/IssueHeader/Buttons/MilestonesButton.js
+++ client/src/components/issues/IssueHeader/Buttons/MilestonesButton.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import S from './style';
+import {MilestoneIcon} from '@primer/octicons-react'
 
 function MilestonesButton (){
     return (
         <S.MilestonesButton>
+            <MilestoneIcon />
             Milestones
         </S.MilestonesButton>
     );
