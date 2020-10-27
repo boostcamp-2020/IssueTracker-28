120c2b9c540262b161db1bf830547331841d80b6
johnyejin <johnyejin@kakao.com>
2020-10-27 11:45:36 +0900

    [Feat] sever 폴더 구조 생성

--- server/routes/auth/index.js
+++ server/routes/auth/index.js
@@ -2,9 +2,9 @@ const express = require('express');
 
 const router = express.Router();
 
-/* GET users listing. */
+/* GET home page. */
 router.get('/', function (req, res, next) {
-  res.send('respond with a resource');
+  res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
