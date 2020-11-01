f8f9ef2cd6c9fe4911f27ba6f3e3566de0fa3886
이상경 <sknglee22@naver.com>
2020-11-01 22:22:39 +0900

    [Fix] ListHeader dropdown item 배열을 map으로 컴포넌트화

--- client/src/components/issues/IssueList/ListHeader/index.js
+++ client/src/components/issues/IssueList/ListHeader/index.js
@@ -1,6 +1,29 @@
-import React from 'react';
+import React, {Fragment} from 'react';
 import { Dropdown } from 'semantic-ui-react';
 import { ListWrapper, ListFilters, FilterDropdown } from './style';
+const AUTHOR_MENU = [
+  'Peter Kim',
+  'Crong Go',
+  'johnyejin'
+];
+const LABEL_MENU = [
+  'Unlabeled',
+  'front-end',
+  'back-end',
+  'Bug',
+  'High-priority',
+];
+const MILESTONE_MENU = [
+  'Issues with no milestone',
+  '스프린트 1',
+  '스프린트 2',
+];
+const ASSIGNEE_MENU = [
+  'Assigned to nobody',
+  'Peter Kim',
+  'johnyejin'
+];
+
 
 function ListHeader() {
   return (
@@ -11,7 +34,12 @@ function ListHeader() {
           <Dropdown className='author-dropdown dropdown' text='Author'>
             <Dropdown.Menu className="dropdown-menu" direction='left'>
               <Dropdown.Header className="dropdown-header" content='Filter by author' />
-              <Dropdown.Item className="dropdown-item" text='johnyejin' />
+                {AUTHOR_MENU.map((item, index) => (
+                  <Fragment>
+                    <hr className="dropdown-divider"/>
+                    <Dropdown.Item className="dropdown-item" text={item} key={index} />
+                  </Fragment>
+                ))}
             </Dropdown.Menu>
           </Dropdown>
         </FilterDropdown>
@@ -19,7 +47,12 @@ function ListHeader() {
           <Dropdown className='label-dropdown dropdown' text='Label'>
             <Dropdown.Menu className="dropdown-menu" direction='left'>
               <Dropdown.Header className="dropdown-header" content='Filter by label' />
-              <Dropdown.Item className="dropdown-item" text='frontend' />
+              {LABEL_MENU.map((item, index) => (
+                  <Fragment>
+                    <hr className="dropdown-divider"/>
+                    <Dropdown.Item className="dropdown-item" text={item} key={index} />
+                  </Fragment>
+                ))}
             </Dropdown.Menu>
           </Dropdown>
         </FilterDropdown>
@@ -27,7 +60,12 @@ function ListHeader() {
           <Dropdown className='milestons-dropdown dropdown' text='Milestons'>
             <Dropdown.Menu className="dropdown-menu" direction='left'>
               <Dropdown.Header className="dropdown-header" content='Filter by milestons' />
-              <Dropdown.Item className="dropdown-item" text='스프린트1' />
+              {MILESTONE_MENU.map((item, index) => (
+                  <Fragment>
+                    <hr className="dropdown-divider"/>
+                    <Dropdown.Item className="dropdown-item" text={item} key={index} />
+                  </Fragment>
+                ))}
             </Dropdown.Menu>
           </Dropdown>
         </FilterDropdown>
@@ -35,7 +73,12 @@ function ListHeader() {
           <Dropdown className='assignee-dropdown dropdown' text='Assignee'>
             <Dropdown.Menu className="dropdown-menu" direction='left'>
               <Dropdown.Header className="dropdown-header" content='Filter by assignee' />
-              <Dropdown.Item className="dropdown-item" text='johnyejin' />
+              {ASSIGNEE_MENU.map((item, index) => (
+                  <Fragment>
+                    <hr className="dropdown-divider"/>
+                    <Dropdown.Item className="dropdown-item" text={item} key={index} />
+                  </Fragment>
+                ))}
             </Dropdown.Menu>
           </Dropdown>
         </FilterDropdown>
