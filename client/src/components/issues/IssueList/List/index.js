184b9923cb3f66c33ca530bec29a3b3ba73b2348
이상경 <sknglee22@naver.com>
2020-11-03 20:42:31 +0900

    [Feat] 필터링 결과 보여줄 목록이 없는 경우 처리
    -필터링 결과 보여줄 목록이 없는 경우 따로 화면처리를 해서 보여준다

--- client/src/components/issues/IssueList/List/index.js
+++ client/src/components/issues/IssueList/List/index.js
@@ -3,6 +3,8 @@ import Issue from './Issue';
 import { useIssuesState, useIssuesDispatch, getIssues } from '@contexts/IssuesContext';
 import {filterIssue} from '../../../../util/filterIssue'
 import { test_items } from './testItems'
+import {IssueOpenedIcon} from '@primer/octicons-react'
+import {NoResultsBox} from './style.js';
 
 
 function List() {
@@ -20,19 +22,24 @@ function List() {
 
   useEffect(() => {
     fetchData();
-  }, [dispatch]);
+  }, []);
 
   if (loading) return <div> 로딩중.. </div>;
   if (error) return <div> 에러가 발생했습니다 </div>;
   if (!issues) return <button onClick={fetchData}> 불러오기 </button>;
 
+  const filteredIssues = test_items.filter((issue)=>{
+    return filterIssue(issue, filters)
+    }).map((issue) => (
+    <Issue key={issue.id} issue={issue} />
+  ))
+
   return (
     <div className="list-wrapper">
-      {test_items.filter((issue)=>{
-        return filterIssue(issue, filters)
-      }).map((issue) => (
-        <Issue key={issue.id} issue={issue} />
-      ))}
+      {filteredIssues.length > 0  ? 
+        filteredIssues :
+        <NoResultsBox><IssueOpenedIcon size={35} className="issue-opened-icon"/>No results matched your search.</NoResultsBox>
+      }
     </div>
   );
 }
