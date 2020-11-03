e42b110f17e8f066a764dc9fc60d8b4737c5ef35
johnyejin <johnyejin@kakao.com>
2020-11-03 22:32:54 +0900

    [Style] assignee 컴포넌트 생성

--- client/src/components/input/index.js
+++ client/src/components/input/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Assignee from '../assignee';
 import Label from '../label';
 import Milestone from '../milestone';
 
 function Input() {
   return (
     <>
+      <Assignee />
       <Label />
       <Milestone />
     </>
