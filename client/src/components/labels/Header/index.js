6c0f47b1c5ada36ba066cb2f1a83292998f8bc5b
이상경 <sknglee22@naver.com>
2020-11-08 17:22:07 +0900

    [Feat] label,milestone 탭 페이지 연결

--- client/src/components/labels/Header/index.js
+++ client/src/components/labels/Header/index.js
@@ -1,20 +1,21 @@
-import React, {useState} from 'react';
+import React from 'react';
+import { useHistory } from 'react-router-dom';
 import { TagIcon, MilestoneIcon } from '@primer/octicons-react';
 import S from './style';
 
 function Header({isCreateState, setIsCreateState}) {
+  const history = useHistory();
   const handleIsCreate=()=>{
     setIsCreateState(!isCreateState);
   }
-
   return (
     <S.LabelHeader>
       <S.LabelMilestone>
-        <S.LabelsButton>
+        <S.LabelsButton onClick={()=>history.push('/label')}>
           <TagIcon size={14} />
           <S.ButtonText>Labels</S.ButtonText>
         </S.LabelsButton>
-        <S.MilestonesButton>
+        <S.MilestonesButton onClick={()=>history.push('/milestone')}>
           <MilestoneIcon />
           <S.ButtonText>Milestones</S.ButtonText>
         </S.MilestonesButton>
--- client/src/components/milestones/Header/index.js
+++ client/src/components/milestones/Header/index.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import { TagIcon, MilestoneIcon } from '@primer/octicons-react';
+import { useHistory } from 'react-router-dom';
 import S from './style';
 
 function Header() {
+  const history = useHistory();
   return (
     <S.MilestoneHeader>
       <S.LabelMilestone>
-        <S.LabelsButton>
+        <S.LabelsButton onClick={()=>history.push('/label')}>
           <TagIcon size={14} />
           <S.ButtonText>Labels</S.ButtonText>
         </S.LabelsButton>
-        <S.MilestonesButton>
+        <S.MilestonesButton onClick={()=>history.push('/milestone')}>
           <MilestoneIcon />
           <S.ButtonText>Milestones</S.ButtonText>
         </S.MilestonesButton>
