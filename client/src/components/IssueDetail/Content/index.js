51ca5d3c59923f9013b8c237e74c16f330cafe17
yeji <yeji9175@naver.com>
2020-11-10 11:01:15 +0900

    [Fix] 오류 해결

--- client/src/components/IssueDetail/Content/index.js
+++ client/src/components/IssueDetail/Content/index.js
@@ -3,14 +3,14 @@ import MainContent from '@components/issueDetail/content/mainContent';
 import Sidebar from '@components/sidebar';
 import S from './style';
 
-const Content = () => {
+const Content = ({ issue, comments }) => {
   const [selectedAssignees, setSelectedAssignees] = useState(new Set());
   const [selectedLabels, setSelectedLabels] = useState(new Set());
   const [selectedMilestone, setSelectedMilestone] = useState(null);
 
   return (
     <S.ContentWrapper>
-      <MainContent />
+      <MainContent issue={issue} comments={comments} />
       <Sidebar
         selectedAssignees={selectedAssignees}
         setSelectedAssignees={setSelectedAssignees}
