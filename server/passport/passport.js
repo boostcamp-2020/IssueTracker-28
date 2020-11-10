deb29559aee965237be3c29e733605c3db0698d8
이상경 <sknglee22@naver.com>
2020-11-10 11:21:05 +0900

    ..

--- server/passport/passport.js
+++ server/passport/passport.js
@@ -3,33 +3,33 @@ const { Strategy: GithubStrategy } = require('passport-github2');
 const UserServices = require('../services/user');
 
 const githubConfig = {
-  clientID: process.env.GITHUB_CLIENT_ID,
-  clientSecret: process.env.GITHUB_CLIENT_SECRET,
-  callbackURL: process.env.GITHUB_CALLBACK_URL,
+    clientID: process.env.GITHUB_CLIENT_ID,
+    clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    callbackURL: process.env.GITHUB_CALLBACK_URL,
 };
 
-const githubLoginVerify = async (accessToken, refreshToken, profile, done) => {
-  try {
-    const {
-      _json: { login },
-    } = profile;
-    const user = await UserServices.findUser(login);
-    if (user) return done(null, user);
-    const addUser = await UserServices.insertUser(login);
-    return done(null, addUser.dataValues);
-  } catch (err) {
-    return done(null, false, { msg: '올바르지 않은 인증정보 입니다.' });
-  }
+const githubLoginVerify = async(accessToken, refreshToken, profile, done) => {
+    try {
+        const {
+            _json: { login },
+        } = profile;
+        const user = await UserServices.findUser(login);
+        if (user) return done(null, user);
+        const addUser = await UserServices.insertUser(login);
+        return done(null, addUser.dataValues);
+    } catch (err) {
+        return done(null, false, { msg: '올바르지 않은 인증정보 입니다.' });
+    }
 };
 
 passport.serializeUser((user, done) => {
-  done(null, user);
+    done(null, user);
 });
 
 passport.deserializeUser((user, done) => {
-  done(null, user);
+    done(null, user);
 });
 
 module.exports = () => {
-  passport.use('github', new GithubStrategy(githubConfig, githubLoginVerify));
+    passport.use('github', new GithubStrategy(githubConfig, githubLoginVerify));
 };
