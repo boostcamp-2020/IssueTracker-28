ada45e4d6a47cb59a87123c6a3b219c8687be073
sang-gyeong lee <60839959+sang-gyeong@users.noreply.github.com>
2020-11-11 10:48:19 +0900

    Update uncheckAllFilters.js

--- client/src/utils/uncheckAllFilters.js
+++ client/src/utils/uncheckAllFilters.js
@@ -5,9 +5,3 @@ export const uncheckAllFilters = (parentNode) => {
     if (checkIcon.classList.contains('show')) checkIcon.classList.remove('show')
   })
 }
-  const parent = parentNode ? parentNode : document;
-  const checkIcons = parent.querySelectorAll('.check-icon');
-  checkIcons.forEach((checkIcon) => {
-    if (checkIcon.classList.contains('show')) checkIcon.classList.remove('show');
-  });
-};
